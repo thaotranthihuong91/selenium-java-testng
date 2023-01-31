202e3e6077ea2c0ffdc8d3dce9f906fc44784895
Thao Tran <thao.tranthihuong.91@gmail.com>
2023-02-01 00:48:55 +0700

    Topic 31 (User Interaction - Part I)

---
+++ src/webdriver/Topic_13_Action_PartI.java
@@ -0,0 +1,82 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_13_Action_PartI {
+	WebDriver driver;
+	Actions action;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Mac OS")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		}
+
+		driver = new FirefoxDriver();
+		action = new Actions(driver);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	
+	public void TC_01_Tooltip() {
+		driver.get("https://automationfc.github.io/jquery-tooltip/");
+		action.moveToElement(driver.findElement(By.cssSelector("input#age"))).perform();
+		sleepInSecond(3);
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.ui-tooltip")).getText(),
+				"We ask for your age only for statistical purposes.");
+	}
+
+	
+	public void TC_02_() {
+		driver.get("http://www.myntra.com/");
+		action.moveToElement(driver.findElement(By.xpath("//div[@class='desktop-navLink']/a[text()='Kids']"))).perform();
+		sleepInSecond(3);
+		driver.findElement(By.xpath("//div[@class='desktop-navLink']//a[text()='Home & Bath']")).click();
+		Assert.assertEquals(driver.findElement(By.cssSelector("span.breadcrumbs-crumb")).getText(), "Kids Home Bath");
+	}
+
+	@Test
+	public void TC_03_() {
+		driver.get("https://www.fahasa.com/");
+		sleepInSecond(5);
+		
+		action.moveToElement(driver.findElement(By.cssSelector("span.icon_menu"))).perform();
+		sleepInSecond(3);
+		
+		action.moveToElement(driver.findElement(By.xpath("//div[contains(@class,'fhs_menu_title')]//span[text()='Sách Trong Nước']"))).perform();
+		sleepInSecond(2);
+		
+		driver.findElement(By.xpath("//div[contains(@class,'fhs_menu_content')]//a[text()='Quản Trị - Lãnh Đạo']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//ol[@class='breadcrumb']//strong[text()='Quản Trị - Lãnh Đạo']")).isDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	// Sleep cứng (Static wait)
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
